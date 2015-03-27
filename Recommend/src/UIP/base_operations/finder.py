f181c5764b9387026d04f51f81fdf342cc262455
Tushar Goswami <tushar@solutionbeyond.net>
2015-03-27 16:42:49 +0530

    support for RemoveX CommandNet

--- Recommend/src/UIP/base_operations/finder.py
+++ Recommend/src/UIP/base_operations/finder.py
@@ -704,7 +704,7 @@ class Finder():
                     final_nodes.append(node)
         return user_tokens, final_nodes
     
-    def processRemove(self,parent_node):
+    def processRemoveX(self,parent_node):
         # @TODO : Logics for this part need to be prepared. It will be coded later
         user_tokens = []
         item_nodes = []
