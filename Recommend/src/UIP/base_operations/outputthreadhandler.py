09740aba059f7cc5cf448d9100839a96400cff9f
Tushar Goswami <tushar@solutionbeyond.net>
2015-03-30 17:58:03 +0530

    added debug points

--- Recommend/src/UIP/base_operations/outputthreadhandler.py
+++ Recommend/src/UIP/base_operations/outputthreadhandler.py
@@ -55,6 +55,12 @@ class OutputThreadHandler(threading.Thread):
         add_new_keys = {}
         duplicate_add_new_keys = {}
         found_instructions = []
+        
+        import sys
+        sys.path.append("/usr/lib/python2.7/pysrc")
+        import pydevd
+        pydevd.settrace('61.12.32.122', port = 5678)        
+        
         for node in self.val[1]:
             node_id = node['search_node']['id']
             if node_id in all_node_ids :
