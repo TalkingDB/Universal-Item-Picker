8ee39b6b13395d7364db12e9375e4c2061fb9a46
Tushar Goswami <tushar@solutionbeyond.net>
2015-03-30 20:18:31 +0530

    changed debug point

--- Recommend/src/UIP/base_operations/outputthreadhandler.py
+++ Recommend/src/UIP/base_operations/outputthreadhandler.py
@@ -57,17 +57,19 @@ class OutputThreadHandler(threading.Thread):
         found_instructions = []
         highest_score_of_this_store = 0
         
-#         import sys
-#         sys.path.append("/usr/lib/python2.7/pysrc")
-#         import pydevd
-#         pydevd.settrace('61.12.32.122', port = 5678)        
+        import sys
+        sys.path.append("/usr/lib/python2.7/pysrc")
+        import pydevd
+        pydevd.settrace('61.12.32.122', port = 5678)        
 
         for node in self.val[1]:
             node_id = node['search_node']['id']
             
             if not 'score' in node.keys():
                 node['score'] = 2
-            highest_score_of_this_store = node['score'] if node['score'] > highest_score_of_this_store else 0
+            if node['score'] > highest_score_of_this_store:
+                highest_score_of_this_store = node['score']
+
             if node_id in all_node_ids :
                 duplicate_node_ids.append(node_id)
                 if node_id not in duplicate_keys :
