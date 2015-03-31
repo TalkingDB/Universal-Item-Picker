3fa943bd884774e337fd2f920fe58212b8b9348c
Tushar Goswami <tushar@solutionbeyond.net>
2015-03-31 12:54:41 +0530

    sorted stores with score - test1

--- Recommend/src/recommendation.py
+++ Recommend/src/recommendation.py
@@ -35,7 +35,7 @@ class Recommendation():
         result_stores_and_their_items_with_options = instruction_parser.parseInstruction(node_ids,json_data, self.concept_space)
         print '[' + str(datetime.datetime.now()) + '] executed parseInstruction in recommendation.py/generateRecommendation'
         
-        self.__sortWithScore(result_stores_and_their_items_with_options)
+        self.__sortStoreWithScore(result_stores_and_their_items_with_options)
         
         return result_stores_and_their_items_with_options
     
@@ -45,18 +45,36 @@ class Recommendation():
         self.values = params['parent']['value']
         self.instruction = params['instruction']
     
-    def __sortWithScore(self,stores_and_items):
+    def __sortStoreWithScore(self,stores_and_items):
         import sys
         sys.path.append("/usr/lib/python2.7/pysrc")
         import pydevd
         pydevd.settrace('61.12.32.122', port = 5678)
         
+        highest_score_for_particular_instruction = {} #for each instruction, it stores the highest score attained by an item
+        tmp_stores_and_items = []
+         
         for store in stores_and_items:
-            store['score']
+            store_item_groups = store['children'] 
+            for item_group in store_item_groups:
+                store_items = item_group['children']
+                for store_item in store_items:
+                    instruction = store_item['instruction']
+                    if not 'instruction' in highest_score_for_particular_instruction.keys():
+                        highest_score_for_particular_instruction[instruction] = store_item['score']
+                    else:
+                        if highest_score_for_particular_instruction[instruction] < store_item['score']:
+                            highest_score_for_particular_instruction[instruction] = store_item['score']
+            
+            store_score = sum(highest_score_for_particular_instruction.values())
+                
+            store['score'] = store_score
+            tmp_stores_and_items.append(store)
         
-        print stores_and_items
+        sorted_stores =  sorted(tmp_stores_and_items,key=lambda particular_store: particular_store['score'])
         
- 
+        return sorted_stores
+    
 # r = Recommendation()
 #  
 # test =  r.generateRecommendation({'instruction': 'wonton soup~fries', 'parent': {u'value': [u'72894', u'81443', u'70681', u'30857', u'52951', u'73498', u'70263', u'68964', u'29602', u'79779', u'65010', u'68924', u'46911', u'75328', u'79960', u'38911', u'74661', u'71519', u'70131', u'69384', u'75272', u'73597', u'64309', u'64265', u'69210', u'71414', u'73185', u'68291', u'73578', u'60176', u'75342', u'73027', u'71099', u'73039', u'74101', u'58898', u'66713', u'70377', u'74661', u'70131', u'71519', u'65142', u'68924', u'72288', u'3028', u'79963', u'66109', u'72287', u'73578', u'70727', u'80415', u'31687', u'71093', u'72905', u'79753', u'80237', u'80229', u'79540', u'75297', u'74541', u'71913', u'71022', u'73926', u'74845', u'69052', u'68660', u'75449', u'71677', u'69984', u'64758', u'70029', u'75209', u'79021', u'77734', u'77748', u'71064', u'71274', u'71059', u'71028', u'76851', u'75653', u'75511', u'69917', u'60438', u'62171', u'70778', u'62335'], u'key': u'uid'}, 'concept_space': u'foodweasel.com'})
